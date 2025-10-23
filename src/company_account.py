d6079753e728df31d4cdd1ca8cba45ad7c0beda6
Dawid Podhrebelny <dpodhrebelny@INF.UG.EDU.PL>
2025-10-23 13:54:18 +0200

    Add feature 7 - company accounts

---
+++ src/company_account.py
@@ -0,0 +1,12 @@
+from src.account import Account
+
+class CompanyAccount(Account):
+    def __init__(self, company_name, nip):
+        super().__init__()
+        self.company_name = company_name
+        self.nip = nip if self.is_nip_valid(nip) else "Invalid"
+
+    def is_nip_valid(self, nip):
+        if isinstance(nip, str) and len(nip) == 10:
+            return True
+        return False
--- tests/unit/test_transfers.py
+++ tests/unit/test_transfers.py
@@ -2,28 +2,28 @@ from src.account import Account
 
 class TestTransfers:
     def test_incoming_transfer(self):
-        account = Account("John", "Doe", "12345678910")
+        account = Account()
         account.incoming_transfer(75.0)
         assert account.balance == 75.0
 
     def test_incoming_transfer_negative_amount(self):
-        account = Account("John", "Doe", "12345678910")
+        account = Account()
         account.incoming_transfer(-75.0)
         assert account.balance == 0.0
 
     def test_outgoing_transfer(self):
-        account = Account("John", "Doe", "12345678910")
+        account = Account()
         account.balance = 100.0
         account.outgoing_transfer(75.0)
         assert account.balance == 25.0
 
     def test_outgoing_transfer_negative_amount(self):
-        account = Account("John", "Doe", "12345678910")
+        account = Account()
         account.outgoing_transfer(-75.0)
         assert account.balance == 0.0
 
     def test_outgoing_transfer_exceeding_balance(self):
-        account = Account("John", "Doe", "12345678910")
+        account = Account()
         account.balance = 100.0
         account.outgoing_transfer(175.0)
         assert account.balance == 100.0
