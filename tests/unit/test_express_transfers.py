3dfeac48b2d054ac3826f730b2b4ae8408b138b1
Dawid Podhrebelny <d.podhrebelny.216@studms.ug.edu.pl>
2025-11-19 11:39:40 +0100

    Refactor express transfer tests

--- tests/unit/test_express_transfers.py
+++ tests/unit/test_express_transfers.py
@@ -1,51 +1,52 @@
+import pytest
 from src.company_account import CompanyAccount
 from src.personal_account import PersonalAccount
 
 class TestCompanyExpressTransfers:
-    def test_outgoing_company_express_transfer(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.balance = 100.0
-        account.outgoing_express_transfer(75.0)
-        assert account.balance == 20.0
-
-    def test_outgoing_company_express_transfer_negative_amount(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.outgoing_express_transfer(-75.0)
-        assert account.balance == 0.0
-
-    def test_outgoing_company_express_transfer_exceeding_balance(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.balance = 100.0
-        account.outgoing_express_transfer(175.0)
-        assert account.balance == 100.0
+    @pytest.fixture(autouse=True)
+    def account(self):
+        return CompanyAccount("Amuse Incorporated", "1234567890")
     
-    def test_outgoing_company_express_transfer_exceeding_balance_by_fee(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.balance = 100.0
-        account.outgoing_express_transfer(100.0)
-        assert account.balance == -5.0
+    @pytest.mark.parametrize("initial_balance, transfer_value, expected_balance", [
+        (100.0, 75.0, 20.0),
+        (100.0, -75.0, 100.0),
+        (100.0, True, 100.0),
+        (200.0, 275.0, 200.0),
+        (500.0, 500.0, -5.0)
+    ],
+    ids=[
+        "normal transfer",
+        "negative value",
+        "not number value",
+        "exceeding balance",
+        "exceeding balance by fee"
+    ])
+    def test_outgoing_company_express_transfer(self, account, initial_balance, transfer_value, expected_balance):
+        account.balance = initial_balance
+        account.outgoing_express_transfer(transfer_value)
+        assert account.balance == expected_balance
 
 
 class TestPersonalExpressTransfers:
-    def test_outgoing_personal_express_transfer(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.balance = 100.0
-        account.outgoing_express_transfer(75.0)
-        assert account.balance == 24.0
-
-    def test_outgoing_personal_express_transfer_negative_amount(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.outgoing_express_transfer(-75.0)
-        assert account.balance == 0.0
-
-    def test_outgoing_personal_express_transfer_exceeding_balance(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.balance = 100.0
-        account.outgoing_express_transfer(175.0)
-        assert account.balance == 100.0
+    @pytest.fixture(autouse=True)
+    def account(self):
+        return PersonalAccount("John", "Doe", "12345678910")
     
-    def test_outgoing_personal_express_transfer_exceeding_balance_by_fee(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.balance = 100.0
-        account.outgoing_express_transfer(100.0)
-        assert account.balance == -1.0
+    @pytest.mark.parametrize("initial_balance, transfer_value, expected_balance", [
+        (100.0, 75.0, 24.0),
+        (100.0, -75.0, 100.0),
+        (100.0, True, 100.0),
+        (200.0, 275.0, 200.0),
+        (500.0, 500.0, -1.0)
+    ],
+    ids=[
+        "normal transfer",
+        "negative value",
+        "not number value",
+        "exceeding balance",
+        "exceeding balance by fee"
+    ])
+    def test_outgoing_personal_express_transfer(self, account, initial_balance, transfer_value, expected_balance):
+        account.balance = initial_balance
+        account.outgoing_express_transfer(transfer_value)
+        assert account.balance == expected_balance
