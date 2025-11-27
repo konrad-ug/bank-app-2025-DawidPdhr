0b67229a675096a6369d8189ea7ba93142eca2da
Dawid Podhrebelny <d.podhrebelny.216@studms.ug.edu.pl>
2025-11-27 09:12:28 +0100

    Refactor transactions history tests

--- tests/unit/test_transaction_history.py
+++ tests/unit/test_transaction_history.py
@@ -1,125 +1,90 @@
+import pytest
 from src.company_account import CompanyAccount
 from src.personal_account import PersonalAccount
 
 class TestCompanyTransfersHistory:
-    def test_fresh_company_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        assert account.history == []
-
-    def test_outgoing_company_express_transfer_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.balance = 100.0
-        account.outgoing_express_transfer(75.0)
-        assert account.history == [-75.0, -5.0]
-
-    def test_outgoing_company_express_transfer_negative_amount_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.outgoing_express_transfer(-75.0)
-        assert account.history == []
-
-    def test_outgoing_company_express_transfer_exceeding_balance_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.balance = 100.0
-        account.outgoing_express_transfer(175.0)
-        assert account.history == []
+    @pytest.fixture(autouse=True)
+    def account(self):
+        return CompanyAccount("Amuse Incorporated", "1234567890")
     
-    def test_outgoing_company_express_transfer_exceeding_balance_by_fee_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.balance = 100.0
-        account.outgoing_express_transfer(100.0)
-        assert account.history == [-100.0, -5.0]
-
-    def test_company_incoming_transfer_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.incoming_transfer(75.0)
-        assert account.history == [75.0]
-
-    def test_company_incoming_transfer_negative_amount_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.incoming_transfer(-75.0)
-        assert account.history == []
-
-    def test_company_outgoing_transfer_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.balance = 100.0
-        account.outgoing_transfer(75.0)
-        assert account.history == [-75.0]
-
-    def test_company_outgoing_transfer_negative_amount_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.outgoing_transfer(-75.0)
-        assert account.history == []
-
-    def test_company_outgoing_transfer_exceeding_balance_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.balance = 100.0
-        account.outgoing_transfer(175.0)
-        assert account.history == []
-    
-    def test_company_incoming_and_express_outgoing_transfer_history(self):
-        account = CompanyAccount("Amuse Incorporated", "1234567890")
-        account.incoming_transfer(500.0)
-        account.outgoing_express_transfer(300.0)
-        assert account.history == [500.0, -300.0, -5.0]
+    @pytest.mark.parametrize("initial_balance, incoming_list, outgoing_list, outgoing_express_list, expected_history", [
+        (0.0, [], [], [], []),
+        (100.0, [], [], [75.0], [-75.0, -5.0]),
+        (100.0, [], [], [-75.0], []),
+        (100.0, [], [], [175.0], []),
+        (100.0, [], [], [100.0], [-100.0, -5.0]),
+        (0.0, [75.0], [], [], [75.0]),
+        (0.0, [-75.0], [], [], []),
+        (100.0, [], [75.0], [], [-75.0]),
+        (100.0, [], [-75.0], [], []),
+        (100.0, [], [175.0], [], []),
+        (0.0, [500.0], [100.0], [300.0], [500.0, -100.0, -300.0, -5.0]),
+    ],
+    ids=[
+        "fresh",
+        "outgoing express transfer",
+        "outgoing express transfer with negative amount",
+        "outgoing express transfer exceeding balance",
+        "outgoing express transfer exceeding balance by fee",
+        "incoming transfer",
+        "incoming transfer with negative amount",
+        "outgoing transfer",
+        "outgoing transfer with negative amount",
+        "outgoing transfer exceeding balance",
+        "incoming, outgoing and express outgoing transfers"
+    ])
+    def test_company_history(self, account, initial_balance, incoming_list, outgoing_list, outgoing_express_list, expected_history):
+        account.balance = initial_balance
+        for transfer in incoming_list:
+            account.incoming_transfer(transfer)
+        
+        for transfer in outgoing_list:
+            account.outgoing_transfer(transfer)
+        
+        for transfer in outgoing_express_list:
+            account.outgoing_express_transfer(transfer)
+        assert account.history == expected_history
 
 
 class TestPersonalTransfersHistory:
-    def test_fresh_personal_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        assert account.history == []
-
-    def test_outgoing_personal_express_transfer_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.balance = 100.0
-        account.outgoing_express_transfer(75.0)
-        assert account.history == [-75.0, -1.0]
-
-    def test_outgoing_personal_express_transfer_negative_amount_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.outgoing_express_transfer(-75.0)
-        assert account.history == []
-
-    def test_outgoing_personal_express_transfer_exceeding_balance_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.balance = 100.0
-        account.outgoing_express_transfer(175.0)
-        assert account.history == []
+    @pytest.fixture(autouse=True)
+    def account(self):
+        return PersonalAccount("John", "Doe", "12345678910")
     
-    def test_outgoing_personal_express_transfer_exceeding_balance_by_fee_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.balance = 100.0
-        account.outgoing_express_transfer(100.0)
-        assert account.history == [-100.0, -1.0]
-    
-    def test_personal_incoming_transfer_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.incoming_transfer(75.0)
-        assert account.history == [75.0]
-
-    def test_personal_incoming_transfer_negative_amount_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.incoming_transfer(-75.0)
-        assert account.history == []
-
-    def test_personal_outgoing_transfer_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.balance = 100.0
-        account.outgoing_transfer(75.0)
-        assert account.history == [-75.0]
-
-    def test_personal_outgoing_transfer_negative_amount_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.outgoing_transfer(-75.0)
-        assert account.history == []
-
-    def test_personal_outgoing_transfer_exceeding_balance_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.balance = 100.0
-        account.outgoing_transfer(175.0)
-        assert account.history == []
-    
-    def test_personal_incoming_and_express_outgoing_transfer_history(self):
-        account = PersonalAccount("John", "Doe", "12345678910")
-        account.incoming_transfer(500.0)
-        account.outgoing_express_transfer(300.0)
-        assert account.history == [500.0, -300.0, -1.0]
+    @pytest.mark.parametrize("initial_balance, incoming_list, outgoing_list, outgoing_express_list, expected_history", [
+        (0.0, [], [], [], []),
+        (100.0, [], [], [75.0], [-75.0, -1.0]),
+        (100.0, [], [], [-75.0], []),
+        (100.0, [], [], [175.0], []),
+        (100.0, [], [], [100.0], [-100.0, -1.0]),
+        (0.0, [75.0], [], [], [75.0]),
+        (0.0, [-75.0], [], [], []),
+        (100.0, [], [75.0], [], [-75.0]),
+        (100.0, [], [-75.0], [], []),
+        (100.0, [], [175.0], [], []),
+        (0.0, [500.0], [100.0], [300.0], [500.0, -100.0, -300.0, -1.0]),
+    ],
+    ids=[
+        "fresh",
+        "outgoing express transfer",
+        "outgoing express transfer with negative amount",
+        "outgoing express transfer exceeding balance",
+        "outgoing express transfer exceeding balance by fee",
+        "incoming transfer",
+        "incoming transfer with negative amount",
+        "outgoing transfer",
+        "outgoing transfer with negative amount",
+        "outgoing transfer exceeding balance",
+        "incoming, outgoing and express outgoing transfers"
+    ])
+    def test_personal_history(self, account, initial_balance, incoming_list, outgoing_list, outgoing_express_list, expected_history):
+        account.balance = initial_balance
+        for transfer in incoming_list:
+            account.incoming_transfer(transfer)
+        
+        for transfer in outgoing_list:
+            account.outgoing_transfer(transfer)
+        
+        for transfer in outgoing_express_list:
+            account.outgoing_express_transfer(transfer)
+        assert account.history == expected_history
